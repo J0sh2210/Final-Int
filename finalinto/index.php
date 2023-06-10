b959477b58e90ad2a3d47e21fdedee6c0d317e97
J0sh2210 <132153959+J0sh2210@users.noreply.github.com>
2023-06-10 08:55:12 -0600

    Add files via upload

---
+++ finalinto/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Final into</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+   <h1>Base de datos</h1>
+   <table>
+<tr>Carnet
+     <td>1</td> </tr>
+<tr>Nombre
+    
+</tr>
+<tr>Grado</tr>
+<tr>Telefono</tr>
+
+
+</table>
+</body>
+</html>
+
+<?php
+?>
